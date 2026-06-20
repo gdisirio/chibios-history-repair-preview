2036ed28996e8991bcf3918e747ee098c5c4f897
Giovanni Di Sirio <gdisirio@gmail.com>
2026-06-20 16:24:52 +0200

    Port STM32 ADCv5 driver to XHAL

--- os/xhal/ports/STM32/STM32G0xx/platform.mk
+++ os/xhal/ports/STM32/STM32G0xx/platform.mk
@@ -26,6 +26,7 @@ else
 endif
 
 # Drivers compatible with the platform.
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/ADCv5/driver.mk
 include $(CHIBIOS)/os/xhal/ports/STM32/LLD/DACv1/driver.mk
 include $(CHIBIOS)/os/xhal/ports/STM32/LLD/DMAv1/driver.mk
 include $(CHIBIOS)/os/xhal/ports/STM32/LLD/EXTIv1/driver.mk
